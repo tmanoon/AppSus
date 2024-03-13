c3d816f02a0b446f2faaef6f586a59fe6a08f3b9
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-13 11:45:34 +0200

    Add random color function to util service

--- services/util.service.js
+++ services/util.service.js
@@ -60,3 +60,12 @@ function getMonthName(date) {
     ]
     return monthNames[date.getMonth()]
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
