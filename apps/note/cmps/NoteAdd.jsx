512a1c5513bca0cff7cfebfc0a81378944bf1476
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-14 11:15:17 +0200

    Add pin note to title on searching

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -28,11 +28,11 @@ export function NoteAdd({ setNote, notes }) {
 
 
     return <div className="add-note-div flex justify-center">
-        <div className="input-place">
         <input type="text" placeholder="Take a note..." name="note-edit" onClick={onAddNote} />
+        {isNote && <span className="pin"></span>}
         {!isNote &&<div className="icons"> <span className="square-check" onClick={onAddTodoNote}></span>
         <span className="brush" onClick={onAddCanvasNote}></span>
-        <span className="image" onClick={onAddImageNote}></span></div>}</div>
+        <span className="image" onClick={onAddImageNote}></span></div>}
         {isNote && <div className="add-note-txt"><p contentEditable onInput={handleInput}></p>{content}</div>}
     </div>
 }
