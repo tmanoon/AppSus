869d1f528ab43f1a1c3ceb28cb28499cffb068ae
JennyLottner <jenny.lottner@gmail.com>
2024-03-15 15:42:20 +0200

    Add email unread count

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -24,18 +24,26 @@ export function MailList({ emails, onSetFilter, onRemoveEmail, onUnread, onMarkE
         handleChange(val)
     }
 
+    function unreadCount() {
+        const unreadCount = emails.filter(email => !email.isRead).length
+        const allEmailsCount = emails.length
+        return unreadCount + '/' + allEmailsCount
+    }
 
     if (!emails.length || !emails) return <div className="email-list flex center"><div>No Emails to show</div></div>
     return <ul className="email-list">
-        <li className="email-sort flex">
-            {currSorter.sort === 'date' && <button onClick={() => onClickSort('date')} style={{ backgroundColor: 'rgba(245, 245, 245, 0.9)' }}><span>Date</span></button>}
+        <li className="email-sort flex space-between">
+            <div className="flex space-between">
+            {currSorter.sort === 'date' && <button onClick={() => onClickSort('date')} style={{ backgroundColor: 'rgba(225, 224, 224, 0.8)' }}><span>Date</span></button>}
             {currSorter.sort !== 'date' && <button onClick={() => onClickSort('date')}><span>Date</span></button>}
             {currSorter.sort !== 'subject' && <button onClick={() => onClickSort('subject')}><span>Subject</span></button>}
-            {currSorter.sort === 'subject' && <button onClick={() => onClickSort('subject')} style={{ backgroundColor: 'rgba(245, 245, 245, 0.9)' }}><span>Subject</span></button>}
-            {currSorter.dir === true && <button onClick={() => onClickSort(true)} style={{ backgroundColor: 'rgba(245, 245, 245, 0.9)' }}><i className="fa-solid fa-chevron-up"></i></button>}
+            {currSorter.sort === 'subject' && <button onClick={() => onClickSort('subject')} style={{ backgroundColor: 'rgba(225, 224, 224, 0.8)' }}><span>Subject</span></button>}
+            {currSorter.dir === true && <button onClick={() => onClickSort(true)} style={{ backgroundColor: 'rgba(225, 224, 224, 0.8)' }}><i className="fa-solid fa-chevron-up"></i></button>}
             {currSorter.dir !== true && <button onClick={() => onClickSort(true)}><i className="fa-solid fa-chevron-up"></i></button>}
             {currSorter.dir !== false && <button onClick={() => onClickSort(false)}><i className="fa-solid fa-chevron-down"></i></button>}
-            {currSorter.dir === false && <button onClick={() => onClickSort(false)} style={{ backgroundColor: 'rgba(245, 245, 245, 0.9)' }}><i className="fa-solid fa-chevron-down"></i></button>}
+            {currSorter.dir === false && <button onClick={() => onClickSort(false)} style={{ backgroundColor: 'rgba(225, 224, 224, 0.8)' }}><i className="fa-solid fa-chevron-down"></i></button>}
+            </div>
+            <p>Unread: {unreadCount()}</p>
         </li>
         {emails.map(email => <li key={email.id} className="grid">
             <div className="email-mark-actions flex align-center">
