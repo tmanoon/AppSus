6ff727501acc5b3600555cbdf922ec44a0754835
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-21 04:19:12 +0300

    Edit text and structure of about component

--- views/About.jsx
+++ views/About.jsx
@@ -1,8 +1,8 @@
 
 export function About() {
-    return <section className="about flex column">
-        <h2>About Page</h2>
-        <h4>This app was made with the hard work, blood, sweat and tears of</h4>
-        <h4>Jenny Lottner and Shoval Sabag</h4>
+    return <section className="about flex column center">
+        <h2>About</h2>
+        <p>Welcome to our dynamic to-do list platform, where productivity meets innovation! <br/>Designed to cater to your organizational needs, this intuitive tool not only allows you to manage tasks seamlessly but also supports video notes for a more immersive experience. Much like Google Keep, our platform enables full CRUDL (Create, Read, Update, Delete, and List) functionality, giving you complete control over your tasks and notes.
+            You can categorize, prioritize, and even collaborate on tasks with ease, ensuring nothing falls through the cracks. Plus, with an interface that resembles Gmail, navigating your to-do list feels familiar and user-friendly. Just like your email, our site supports CRUDL operations, allowing you to effortlessly manage your communications alongside your tasks. Whether you’re juggling personal projects or coordinating with a team, our platform empowers you to stay organized, focused, and efficient, taking your productivity to new heights. Embrace a smarter way to manage your life today!</p>
     </section>
 }
